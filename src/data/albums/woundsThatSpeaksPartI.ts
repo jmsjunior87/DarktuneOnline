73c09925ba5be2b7e80e2aef079c40631ec51e8d
jmjunio87 <josemaga98@gmail.com>
2025-08-19 18:37:47 -0300

    Adicionando novos albums

--- src/data/albums/woundsThatSpeaksPartI.ts
+++ src/data/albums/woundsThatSpeaksPartI.ts
@@ -8,105 +8,105 @@ export const woundsThatSpeaksPartI: AlbumData = {
   tracks: [
     {
       id: "1V0QfQZFcZ8tYe3DokKWi0-jkfKZ5JMU4",
-      title: "01 - Shattered Mind",
+      title: "Shattered Mind",
       artist: "Rot",
       trackNumber: 1,
       duration: "5:03"
     },
     {
       id: "1RT1ItxBG8vuRkLIIVsG3l-VqA8hPigJ9",
-      title: "02 - Bled Dry",
+      title: "Bled Dry",
       artist: "Rot",
       trackNumber: 2,
       duration: "5:15"
     },
     {
       id: "1Mu9RPFwoIQzhlgbawRF-MrN2qJEv9mTp",
-      title: "03 - Ashfall",
+      title: "Ashfall",
       artist: "Rot",
       trackNumber: 3,
       duration: "5:03"
     },
     {
       id: "1w36yo9ahE4E-VsONfQxUZJnwZ6XV_VBy",
-      title: "04 - Torn Faith",
+      title: "Torn Faith",
       artist: "Rot",
       trackNumber: 4,
       duration: "5:19"
     },
     {
       id: "1krMKdzbOQWBKVvZNx3PTiJ1ysHrErkdT",
-      title: "05 - Silent Crush",
+      title: "Silent Crush",
       artist: "Rot",
       trackNumber: 5,
       duration: "4:53"
     },
     {
       id: "1_ppd_Bwju5R654Yk9WLjq099Rc8LTeNg",
-      title: "06 - Skin Deep",
+      title: "Skin Deep",
       artist: "Rot",
       trackNumber: 6,
       duration: "4:53"
     },
     {
       id: "1Bm8eleKkmH6Gh-BujS5th2Oi9aBEKS4-",
-      title: "07 - Rot Inside",
+      title: "Rot Inside",
       artist: "Rot",
       trackNumber: 7,
       duration: "4:34"
     },
     {
       id: "1cLa54EZAKxPrOhsj3u8piEtcGVUbegH3",
-      title: "08 - Empty Words",
+      title: "Empty Words",
       artist: "Rot",
       trackNumber: 8,
       duration: "4:36"
     },
     {
       id: "1FEWpePDz2y0qr10t19vJsazQ7mwSWSqb",
-      title: "09 - Fading Away",
+      title: "Fading Away",
       artist: "Rot",
       trackNumber: 9,
       duration: "5:04"
     },
     {
       id: "1Z4XsTx6YoHhr8r1jd-Ls7WUw_54fUoyf",
-      title: "10 - Broken Vows",
+      title: "Broken Vows",
       artist: "Rot",
       trackNumber: 10,
       duration: "5:03"
     },
     {
       id: "1LaDI03xsYtpl4Rn38xWhzkZHoFbn5BhP",
-      title: "11 - Veil of Pain",
+      title: "Veil of Pain",
       artist: "Rot",
       trackNumber: 11,
       duration: "4:35"
     },
     {
       id: "1DQWVxhjYk192lWcPeg0I2-iaZ0i7nhhM",
-      title: "12 - Scarred Truth",
+      title: "Scarred Truth",
       artist: "Rot",
       trackNumber: 12,
       duration: "4:49"
     },
     {
       id: "1pIXinxOuywH9rzXZWVi3CjJlVRXvhO54",
-      title: "13 - Nothing to Hold",
+      title: "Nothing to Hold",
       artist: "Rot",
       trackNumber: 13,
       duration: "4:43"
     },
     {
       id: "1J7QggTZ1CrWR7hICzo0DSRjUoUQPOYhV",
-      title: "14 - Wounds That Speak",
+      title: "Wounds That Speak",
       artist: "Rot",
       trackNumber: 14,
       duration: "3:29"
     },
     {
       id: "1Ej-QXbGi5JhEqwDEX1ITjYzosdTwfOru",
-      title: "15 - Still There, Just Not Yours",
+      title: "Still There, Just Not Yours",
       artist: "Rot",
       trackNumber: 15,
       duration: "5:18"
