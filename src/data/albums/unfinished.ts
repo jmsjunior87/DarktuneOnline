246575c413455a47628946bab1503d8652b32291
jmjunio87 <josemaga98@gmail.com>
2025-09-11 20:04:59 -0300

    Atualizando álbuns

--- src/data/albums/unfinished.ts
+++ src/data/albums/unfinished.ts
@@ -20,75 +20,76 @@ export const unfinished: AlbumData = {
       trackNumber: 2,
       duration: "5:18"
     },
-    {
-      id: "13BFDaLbvLeDu0q5XubQ1veMe-OgKH9Qn",
-      title: "Whispers",
-      artist: "Rot",
-      trackNumber: 3,
-      duration: "3:27"
-    },
+    
     {
       id: "1LS4vQs1BMqt7fm4CzLrNNZj0tdrh_YAh",
       title: "Splinter",
       artist: "Rot",
-      trackNumber: 4,
+      trackNumber: 3,
       duration: "4:58"
     },
     {
       id: "13JXaOhVcgYBY8939YxjrylCJeEehgIg2",
       title: "Porcelain",
       artist: "Rot",
-      trackNumber: 5,
+      trackNumber: 4,
       duration: "5:46"
     },
     {
       id: "1f5Zsi3Z8oINHG2dGzGZ5Ru0Qj1odjt_U",
       title: "Ghost in the Wall",
       artist: "Rot",
-      trackNumber: 6,
+      trackNumber: 5,
       duration: "5:05"
     },
     {
       id: "1oaa6Gtd7Pn7QIgPdsOSk90neGaFjPzQQ",
       title: "Civil War",
       artist: "Rot",
-      trackNumber: 7,
+      trackNumber: 6,
       duration: "4:53"
     },
     {
       id: "14nGp7oy_Dofn4LP987dyojwc9QynGpFC",
       title: "Stain",
       artist: "Rot",
-      trackNumber: 8,
+      trackNumber: 7,
       duration: "5:25"
     },
     {
       id: "1b2zaRYFAWqoPox0RMRmETOLoEGLHPJS8",
       title: "Feedback",
       artist: "Rot",
-      trackNumber: 9,
+      trackNumber: 8,
       duration: "5:09"
     },
     {
       id: "1l3Y6WRsOnZOO1S7Q2wHLX216EKsmsKOP",
       title: "Nail",
       artist: "Rot",
-      trackNumber: 10,
+      trackNumber: 9,
       duration: "4:22"
     },
     {
       id: "1Q6alz1t_xZaTEmHG67TKfRBpYJEcxqth",
       title: "Drowning Man",
       artist: "Rot",
-      trackNumber: 11,
+      trackNumber: 10,
       duration: "6:12"
     },
     {
       id: "1YTjkGxwZI5boNbRa7N89pc1fT0A74Xqg",
       title: "Unfinished",
       artist: "Rot",
-      trackNumber: 12,
+      trackNumber: 11,
       duration: "6:29"
     },
+    {
+      id: "13BFDaLbvLeDu0q5XubQ1veMe-OgKH9Qn",
+      title: "Whispers",
+      artist: "Rot",
+      trackNumber: 12,
+      duration: "3:27"
+    }
   ]
 };
