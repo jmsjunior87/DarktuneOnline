eecb38255f51ea526db876af17a431c0bc36a4aa
jmjunio87 <josemaga98@gmail.com>
2025-09-19 18:52:14 -0300

    Atualizando álbuns

--- src/data/albums/kin.ts
+++ src/data/albums/kin.ts
@@ -8,77 +8,77 @@ export const kin: AlbumData = {
   tracks: [
     {
       id: "10etwp0SDFbS3-hFNNN6zxgdzc9Zz-puh",
-      title: "1- Inheritance",
+      title: "Inheritance",
       artist: "Rot",
       trackNumber: 1,
       duration: "4:22"
     },
     {
       id: "1XqGyg9tI4v7oQdF0YroX_zRM4p21yzmL",
-      title: "2- Kin",
+      title: "Kin",
       artist: "Rot",
       trackNumber: 2,
       duration: "5:50"
     },
     {
       id: "1KLNrRakXRIKXRh2m8wDym-EDFz8L72bm",
-      title: "3- Mother's Teeth",
+      title: "Mother's Teeth",
       artist: "Rot",
       trackNumber: 3,
       duration: "4:19"
     },
     {
       id: "19W2M6JBhJoXomRWho_YlcMOQzJdVW6gD",
-      title: "4- Heirloom",
+      title: "Heirloom",
       artist: "Rot",
       trackNumber: 4,
       duration: "3:30"
     },
     {
       id: "1wbjXRzJWqe7J6ZD0y1bHLSju4ld9Nla1",
-      title: "5- Rotblood",
+      title: "Rotblood",
       artist: "Rot",
       trackNumber: 5,
       duration: "4:27"
     },
     {
       id: "1oGyl1JRcNm4f4pzHcDs7xTVhnuQyP0xd",
-      title: "6- Hollow Names",
+      title: "Hollow Names",
       artist: "Rot",
       trackNumber: 6,
       duration: "4:28"
     },
     {
       id: "1eECmYBSBIirsoH5W8lxmVVq-sAZIFz3E",
-      title: "7- Scabbed Lineage",
+      title: "Scabbed Lineage",
       artist: "Rot",
       trackNumber: 7,
       duration: "4:22"
     },
     {
       id: "1rDW5wMSwa9LRLws9E53QsPcg5wYJIiO-",
-      title: "8- Bruise Silence",
+      title: "Bruise Silence",
       artist: "Rot",
       trackNumber: 8,
       duration: "5:04"
     },
     {
       id: "1Ba62YYg4XinFKV2nS1aoV-c6-J2mzMra",
-      title: "9- Father’s Stain",
+      title: "Father’s Stain",
       artist: "Rot",
       trackNumber: 9,
       duration: "3:47"
     },
     {
       id: "17WeeZ-fJfFvPrAMQFRxA7_RR9cQbQ19y",
-      title: "10- Kin",
+      title: "Kin",
       artist: "Rot",
       trackNumber: 10,
       duration: "3:58"
     },
     {
       id: "1eharGvwHF0pmPz1mmoc-BDoJD0hoG9QY",
-      title: "11- Longing",
+      title: "Longing",
       artist: "Rot",
       trackNumber: 11,
       duration: "5:39"
